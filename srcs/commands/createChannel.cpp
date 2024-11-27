cf50bb8b25296c93b51a4175ea180aeb9298f06d
Simos Tigkas <stigkas@c2r1p9.hive.fi>
2024-11-27 18:14:05 +0200

    JOIN is almost done

--- srcs/commands/createChannel.cpp
+++ srcs/commands/createChannel.cpp
@@ -18,10 +18,25 @@ Channel *Server::createChannel(const std::string &name, Client *creator)
     Channel *channel;
     channel = findChannel(name);
     if (channel != nullptr)
+    {
+        if (find(channel->getClients().begin(), channel->getClients().end(), creator->getUsername()) != channel->getClients().end())
+	    {
+		    std::string errorMsg = "Error: You are already in the channel.\r\n";
+		    send(creator->getSocket(), errorMsg.c_str(), errorMsg.length(), 0);
+		    return ;
+	    }
         return channel;
-    Channel *newChannel = new Channel(name);    
-    newChannel->addClient(creator);
-    newChannel->addAdmin(creator);
+    }
+    Channel *newChannel = new Channel(name);  
+    try {
+        newChannel->addClient(creator);
+        newChannel->addAdmin(creator);
+    }
+    catch (const std::exception &e){
+        std::string msg = "Error: " + std::string(e.what()) + "\r\n";
+        send(creator->getSocket(), msg.c_str(), msg.length(), 0);
+        return;
+    }
     _channels.push_back(newChannel);
     return newChannel;
 }
--- srcs/commands/join.cpp
+++ srcs/commands/join.cpp
@@ -2,8 +2,6 @@
 
 void Server::join(std::string buf, int fd)
 {
-	// handle /join command (check if !_clients[index].get_nickname().empty())
-		// else handle it as client joining for the first time
 	std::cout << "--------------- JOIN -----------------" << std::endl;
 	Client *client = nullptr;
     for (Client *c : _clients) {
@@ -13,48 +11,15 @@ void Server::join(std::string buf, int fd)
         }
     }
 	if (!client) {
-		std::cerr << "Error: Client not found for fd " << fd << std::endl;
+		std::cerr << "Error: Client: " << fd << " not found." << std::endl;
         return;
 	}
 	buf.replace(buf.find("\r"), 1, "");
 	buf.replace(buf.find("\n"), 1, "");
 	std::string chName = buf.substr(5);
 	Channel *channel = createChannel(chName, client);
-	if (find(channel->getClients().begin(), channel->getClients().end(), client) != channel->getClients().end())
-	{
-
-	}
-		
-
-
-
-	std::cout << "Client " << client->getSocket() << " sent join message" << std::endl;
-	// response += "JOIN :\r\n";
-	// send(fd, response.c_str(), response.length(), 0);
+    std::string joinMsg = ":" + client->getNickname() + " JOIN " + channel->getName() + "\r\n";
+    channel->broadcast(joinMsg, client); // Broadcast the join to the channel
+    send(fd, joinMsg.c_str(), joinMsg.length(), 0); // Notify the client
+    std::cout << "Client " << client->getNickname() << " joined channel " << channel->getName() << std::endl;
 }
-
-// void Server::join(std::string response, int fd)
-// {
-//     // Check if the client is already in the channel
-//     if (std::find(channel->getClients().begin(), channel->getClients().end(), client) != channel->getClients().end()) {
-//         std::string errorMsg = "Error: You are already in the channel.\r\n";
-//         send(fd, errorMsg.c_str(), errorMsg.length(), 0);
-//         return;
-//     }
-
-//     // Add the client to the channel
-//     try {
-//         channel->addClient(client);
-//     } catch (const std::exception &e) {
-//         std::string errorMsg = "Error: " + std::string(e.what()) + "\r\n";
-//         send(fd, errorMsg.c_str(), errorMsg.length(), 0);
-//         return;
-//     }
-
-//     // Notify the client and the channel
-//     std::string joinMsg = ":" + client->getNickname() + " JOIN " + channelName + "\r\n";
-//     channel->broadcast(joinMsg, nullptr); // Broadcast the join to the channel
-//     send(fd, joinMsg.c_str(), joinMsg.length(), 0); // Notify the client
-
-//     std::cout << "Client " << client->getNickname() << " joined channel " << channelName << std::endl;
-// }
