ca26ed4a828155aa494fa22047d7988cb7d1d8ad
kmatjuhi <kmatjuhi@student.hive.fi>
2024-12-02 14:39:58 +0200

    fixing list

--- srcs/commands/list.cpp
+++ srcs/commands/list.cpp
@@ -2,52 +2,29 @@
 
 void Server::list(std::string buf, int fd)
 {
-	// std::cout << "--------------- LIST -----------------" << std::endl;
-	std::string name = buf.substr(5);
-
-	// if (hasOwnNick(_clients[index], nick))
-	// 	return ;
-	// if (isNickTaken(_clients, nick))
-	// {
-	// 	sendError("433: " + nick + ":Nickname is already in use", fd);
-	// 	return ;
-	// }
-	// if (!isValidNick(nick))
-	// {
-	// 	sendError("432: " + nick + ":Erroneus nickname", fd);
-	// 	return ;
-	// }
-	// std::cout << "Client " << fd << " set nickname to: " << nick << std::endl;
-	// sendResponse(":" + _clients[index]->getNickname() + " NICK " + nick, fd);
-	// _clients[index]->setNickname(nick);
 	std::cout << "--------------- LIST -----------------" << std::endl;
     std::istringstream iss(buf);
     std::string command, chName;
     iss >> command >> chName;
 
-    std::cout << "|" << chName << "|" << std::endl;
-
-    if (chName == "")
+    if (chName.empty())
     {
         for (size_t i = 0; i < _channels.size(); ++i)
-		    sendResponse(_channels[i]->getName(), fd);
-        return ;
+        {
+            std::string response = _channels[i]->getName() + " " + std::to_string(_channels[i]->getUserCount()) + " " +_channels[i]->getTopic();
+            sendResponse(response, fd);
+        }
+        return;
     }
-    else
-    {
-        sendResponse("channel is not empty", fd);
-    }
-    // while (chName)
-    // {
-    //     chName >> n;
-    //     std::cout << "|" << n << "|" <<std::endl;
-    //     for (size_t i = 0; i < _channels.size(); ++i)
-	//     {
-	// 	    if (_channels[i]->getName() == n)
-	// 		    sendResponse(_channels[i]->getName(), fd);
-	//     }
-    // }
 
-
-    std::cout << "empty" << std::endl;
+    do {
+        for (size_t i = 0; i < _channels.size(); ++i)
+	    {
+		    if (_channels[i]->getName() == chName)
+            {
+                std::string response = _channels[i]->getName() + " " + std::to_string(_channels[i]->getUserCount()) + " " +_channels[i]->getTopic();
+			    sendResponse(response, fd);
+            }
+	    }
+    } while (iss >> chName);
 }
