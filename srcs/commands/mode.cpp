8916d76e86789d08a88b10fa0a353b53c2211ee6
Simos Tigkas <suomikendam@gmail.com>
2024-12-04 12:31:51 +0200

    Admin privileges get set or removed as they should nowgit add srcs/commands/mode.cppclear

--- srcs/commands/mode.cpp
+++ srcs/commands/mode.cpp
@@ -1,39 +1,9 @@
 #include "Server.hpp"
 
-/********* Mandatory MODES **********
-
- ·i: Set/remove Invite-only channel
-
-When enabled, other users will not be 
-able to see you on a WHO output unless they 
-are in the channel themselves.
-
- · t: Set/remove the restrictions of the TOPIC command to channel
- operators
-
-When enabled, users who are not opped or voices cannot 
-modify the channel topic.
-
- · k <key>: Set/remove the channel key (password)
-
-Adds or removes a channel key, aka a password; 
-users will not be able to join the channel 
-without providing the key.
-
- · o <nickname>: Give/take channel operator privilege
-
-Adds or removes the operator status from a nickname.
-
-· l <count>: Set/remove the user limit to channel
-
-Adds, removes or updates the maximum amount of 
-users that may join the channel.
-
-*/
-
 // MODE <your nick>|<channel> <modeString> [<modeParam>]
 void Server::mode(std::string buf, int fd, int index)
 {
+	std::cout << "buf: " << buf <<std::endl;
 	std::cout << "--------------- MODE -----------------" << std::endl;
 	if (!validateClientRegistration(fd, index))
 	{
@@ -41,10 +11,18 @@ void Server::mode(std::string buf, int fd, int index)
 		return ;
 	}
 	std::istringstream iss(buf);
-	std::string command, chName, modeString, modeParam;
+	std::string command, chName, nickname, modeString, modeParam, keyPassword, count;
 	iss >> command >> chName >> modeString;
-	std::getline(iss, modeParam);
-
+	if (modeString[1] != 'i' || modeString[1] != 't')
+	{
+		std::getline(iss, modeParam);
+		if (modeString[1] == 'k')
+			keyPassword = modeParam;
+		else if (modeString[1] == 'o')
+			nickname = modeParam.substr(1);
+		else if (modeString[1] == 'l')
+			count = modeParam;
+	}
 	Channel *channel = findChannel(chName);
 	if (chName.empty() || !channel || !channel->isAdmin(_clients[index]))
 	{
@@ -57,23 +35,7 @@ void Server::mode(std::string buf, int fd, int index)
 		std::cout << "Channel error" << std::endl;
 		return ;
 	}
-	Client *target = getClient(modeParam);
-	// if (!target)
-	// {
-    // 	std::cout << "Error: Client with nickname '" << modeParam << "' not found." << std::endl;
-    // 	sendError("432 MODE :Invalid or unusable name", fd);
-    // 	return;
-	// }
-	// else
-    // 	std::cout << "Found client: " << target->getNickname() << std::endl;
-	// if (channel->isClient(target) < 0) //ERROR HERE
-	// {
-    // 	std::cout << "Error: Target client is not in the channel." << std::endl;
-    // 	sendError("441 MODE :They aren't on that channel", fd);
-    // 	return;
-	// }
-	// else
-    // 	std::cout << "Target client is in the channel: " << target->getNickname() << std::endl;
+	
 	//handle multiple modes
 	//client not in the channel
 	bool plussign = true;
@@ -82,23 +44,60 @@ void Server::mode(std::string buf, int fd, int index)
 		plussign = true;
 	else if (mode == '-')
 		plussign = false;
-	for (char mode : modeString.substr(1))
+	// std::cout << "command: " << command << "\n channel: " << channel << "\n modeString: " << modeString << "\n modeParam: " << modeParam <<std::endl;
+	// std::cout << "chName: " << chName << "\n target: " << target <<std::endl;
+	// exit(0);
+	Client *target = getClient(nickname);
+	for (char mode : modeString)
 	{
 		switch (mode)
 		{
+			// ·i: Set/remove Invite-only channel
+
+			// When enabled, other users will not be 
+			// able to see you on a WHO output unless they 
+			// are in the channel themselves.
 			case 'i':
 				channel->setInviteOnly(plussign);
 				break;
+			//  · t: Set/remove the restrictions of the TOPIC command to channel operators.
+			
+			// When enabled, users who are not opped or voices cannot 
+			// modify the channel topic.
 			case 't':
 				channel->setTopicRestrictions(plussign);
 				break;
+			// · k <key>: Set/remove the channel key (password)
+
+			// Adds or removes a channel key, aka a password; 
+			// users will not be able to join the channel 
+			// without providing the key.
 			case 'k':
 				if (plussign && !modeParam.empty())
-					channel->setChannelKey(modeParam);
+					channel->setChannelKey(keyPassword);
 				else
 					channel->setChannelKey("");
 				break;
+			//  · o <nickname>: Give/take channel operator privilege
+
+			// Adds or removes the operator status from a nickname.
 			case 'o':
+				if (!target)
+				{
+    				std::cout << "Error: Client with nickname '" << nickname << "' not found." << std::endl;
+    				sendError("432 MODE :Invalid or unusable name", fd);
+    				return;
+				}
+				else
+    				std::cout << "Found client: " << target->getNickname() << std::endl;
+				if (channel->isClient(target) < 0)
+				{
+    				std::cout << "Error: Target client is not in the channel." << std::endl;
+    				sendError("441 MODE :They aren't on that channel", fd);
+    				return;
+				}
+				else
+    				std::cout << "Target client is in the channel: " << target->getNickname() << std::endl;
 				if (target != nullptr)
 				{
 					if (plussign && !channel->isAdmin(target))
@@ -112,19 +111,23 @@ void Server::mode(std::string buf, int fd, int index)
 					std::cout << "Invalid or unusable name" << std::endl;
 				}
 				break;
+			// · l <count>: Set/remove the user limit to channel
+
+			// Adds, removes or updates the maximum amount of 
+			// users that may join the channel.
 			case 'l':
 				if (plussign)
 				{
-					if (modeParam.empty() || std::stoi(modeParam) < 0)
+					if (count.empty() || std::stoi(count) < 0)
 					{
-						if (modeParam.empty())
+						if (count.empty())
 							sendError("461 MODE :Not enough parameters for MODE", fd);
 						else
 							sendError("461 MODE :Invalid parameter for MODE", fd);
 						std::cout << "Invalid parameter" << std::endl;
 						return ;
 					}
-					channel->setUserLimit(std::stoi(modeParam));
+					channel->setUserLimit(std::stoi(count));
 				}
 				else
 					channel->setUserLimit(-1);
