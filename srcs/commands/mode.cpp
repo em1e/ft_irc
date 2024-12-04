63c02d6a11ba3b780ecd553e9b62c992455fba19
Simos Tigkas <stigkas@c2r1p3.hive.fi>
2024-12-04 17:09:47 +0200

    segfault is fixed now

--- srcs/commands/mode.cpp
+++ srcs/commands/mode.cpp
@@ -1,5 +1,21 @@
 #include "Server.hpp"
 
+/*·i: Set/remove Invite-only channel
+			When enabled, other users will not be 
+			able to see you on a WHO output unless they 
+			are in the channel themselves.
+ · t: Set/remove the restrictions of the TOPIC command to channel operators.
+			When enabled, users who are not opped or voices cannot 
+			modify the channel topic.
+· k <key>: Set/remove the channel key (password)
+			Adds or removes a channel key, aka a password; 
+			users will not be able to join the channel 
+			without providing the key.
+ · o <nickname>: Give/take channel operator privilege
+	Adds or removes the operator status from a nickname.
+· l <count>: Set/remove the user limit to channel
+	Adds, removes or updates the maximum amount of 
+	users that may join the channel.*/
 // MODE <your nick>|<channel> <modeString> [<modeParam>]
 void Server::mode(std::string buf, int fd, int index)
 {
@@ -44,43 +60,23 @@ void Server::mode(std::string buf, int fd, int index)
 		plussign = true;
 	else if (mode == '-')
 		plussign = false;
-	// std::cout << "command: " << command << "\n channel: " << channel << "\n modeString: " << modeString << "\n modeParam: " << modeParam <<std::endl;
-	// std::cout << "chName: " << chName << "\n target: " << target <<std::endl;
-	// exit(0);
 	std::shared_ptr<Client> target = getClient(modeParam);
 	for (char mode : modeString)
 	{
 		switch (mode)
 		{
-			// ·i: Set/remove Invite-only channel
-
-			// When enabled, other users will not be 
-			// able to see you on a WHO output unless they 
-			// are in the channel themselves.
 			case 'i':
 				channel->setInviteOnly(plussign);
 				break;
-			//  · t: Set/remove the restrictions of the TOPIC command to channel operators.
-			
-			// When enabled, users who are not opped or voices cannot 
-			// modify the channel topic.
 			case 't':
 				channel->setTopicRestrictions(plussign);
 				break;
-			// · k <key>: Set/remove the channel key (password)
-
-			// Adds or removes a channel key, aka a password; 
-			// users will not be able to join the channel 
-			// without providing the key.
 			case 'k':
 				if (plussign && !modeParam.empty())
 					channel->setChannelKey(keyPassword);
 				else
 					channel->setChannelKey("");
 				break;
-			//  · o <nickname>: Give/take channel operator privilege
-
-			// Adds or removes the operator status from a nickname.
 			case 'o':
 				if (!target)
 				{
@@ -111,10 +107,6 @@ void Server::mode(std::string buf, int fd, int index)
 					std::cout << "Invalid or unusable name" << std::endl;
 				}
 				break;
-			// · l <count>: Set/remove the user limit to channel
-
-			// Adds, removes or updates the maximum amount of 
-			// users that may join the channel.
 			case 'l':
 				if (plussign)
 				{
