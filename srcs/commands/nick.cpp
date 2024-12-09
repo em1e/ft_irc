cdb80704e08c898b7e87278ae9987347b7bd56cd
kmatjuhi <kmatjuhi@student.hive.fi>
2024-12-09 09:21:06 +0200

    nick updated

--- srcs/commands/nick.cpp
+++ srcs/commands/nick.cpp
@@ -69,15 +69,15 @@ void Server::nick(std::string buf, int fd, int index)
 	std::cout << "Client " << fd << " set nickname to: " << nick << std::endl;
 	sendResponse(":" + _clients[index]->getNickname() + " NICK " + nick, fd);
 
-	// chane nickname in all channels, maybe needed. Needs testing
-	// for (size_t i = 0; i < _channels.size(); ++i)
-	// {
-	// 	if (_channels[i]->isClient(_clients[index]) != -1)
-	// 	{
-	// 		_channels[i]->broadcast(":" + _clients[index]->getNickname() + " NICK " + nick + "\r\n", _clients[index], 1);
-	// 		_channels[i]->getClient(_channels[i]->isClient(_clients[index]))->setNickname(nick);
-	// 	}
-	// }
+	// change nickname in all channels.
+	for (size_t i = 0; i < _channels.size(); ++i)
+	{
+		if (_channels[i]->isClient(_clients[index]) != -1)
+		{
+			_channels[i]->broadcast(":" + _clients[index]->getNickname() + " NICK " + nick + "\r\n", _clients[index], 1);
+			_channels[i]->getClient(_channels[i]->isClient(_clients[index]))->setNickname(nick);
+		}
+	}
 
 	_clients[index]->setNickname(nick);
 }
