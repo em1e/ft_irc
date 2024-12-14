ab790b4ee33e593a82baac7372017c63d750a7bb
k2matu <kstt90@gmail.com>
2024-12-14 19:55:44 +0200

    fixed so only accept one mode and if somebody puts a user which exist. it will give the correct error code. cant change mode for other users

--- srcs/commands/mode.cpp
+++ srcs/commands/mode.cpp
@@ -58,8 +58,12 @@ void Server::mode(std::string buf, int fd, int index)
 	
 	if (chName[0] != '#')
 	{
-		if (modeString != "+i")
+		if (modeString == "+i")
+			return ;
+		if (searchByUsername(chName) == -1)
 			sendError("502 " + nick + " :Cant change mode for other users", fd);
+		else
+			sendError("403 " + nick + " " + chName + " :No such channel", fd);
 		return ;
 	}
 	
@@ -96,67 +100,83 @@ void Server::mode(std::string buf, int fd, int index)
 
 	std::shared_ptr<Client> target = getClient(modeParam);
 	std::string resMsg = ":localhost 324 " + nick + " " + chName + " " + modeSign;
-	for (size_t i = 1; i < modeString.length(); ++i)
+	if (modeString[2])
 	{
-		char mode = modeString[i];
-		switch (mode)
-		{
-			case 'i':
-				if (plussign != channel->getInviteOnly())
-				{
-					channel->setInviteOnly(plussign);
-					channel->broadcast(resMsg + mode + "\r\n", nullptr, 0);
-				}
-				break;
-			case 't':
-				if (plussign != channel->getTopicRestrictions())
-				{
-					channel->setTopicRestrictions(plussign);
-					channel->broadcast(resMsg + mode + "\r\n", nullptr, 0);
-				}
-				break;
-			case 'k':
-				if (!plussign && plussign == channel->getIsChannelPassword())
-					return ;
-				channel->setChannelKey(plussign, modeParam);
-				if (!plussign)
-					modeParam = "*";
-				channel->broadcast(resMsg + mode  + " " + modeParam + "\r\n", nullptr, 0);
-				break;
-			case 'o':
-				if (!target)
-				{
-					sendError("401 " + nick + " " + modeParam + " :No such nick/channel", fd);
-					return;
-				}
-				if (channel->isClient(target) == -1)
-				{
-					sendError("441 " + nick + " " + modeParam + " " + chName + " :They aren't on that channel", fd);
-					return;
-				}
-				if (plussign && channel->isAdmin(target) == -1)
-					channel->addAdmin(target);
-				else if (!plussign && channel->isAdmin(target) >= 0)
-					channel->removeAdmin(target);
-				channel->broadcast(resMsg + mode + " " + modeParam + "\r\n", nullptr, 0);
-				break;
-			case 'l':
-				if ((!modeParam.empty() && std::stoi(modeParam) < 1) || (!plussign && channel->getUserLimit() == -1))
-					return ;
-				if (plussign)
-					channel->setUserLimit(std::stoi(modeParam));
-				else
-					channel->setUserLimit(-1);
-				channel->broadcast(resMsg + mode + " " + modeParam + "\r\n", nullptr, 0);
-				break;
-			default:
+		modeString = modeString.substr(2);
+		sendError("472 " + nick + " " + modeString + " :is unknown mode char to me", fd);
+		return ;
+	}
+	char mode = modeString[1];
+	switch (mode)
+	{
+		case 'i':
+			if (plussign != channel->getInviteOnly())
 			{
-				sendError("472 " + nick + " :is unknown mode char to me", fd);
-				break;
+				channel->setInviteOnly(plussign);
+				channel->broadcast(resMsg + mode + "\r\n", nullptr, 0);
 			}
+			break;
+		case 't':
+			if (plussign != channel->getTopicRestrictions())
+			{
+				channel->setTopicRestrictions(plussign);
+				channel->broadcast(resMsg + mode + "\r\n", nullptr, 0);
+			}
+			break;
+		case 'k':
+			if (!plussign && plussign == channel->getIsChannelPassword())
+				return ;
+			channel->setChannelKey(plussign, modeParam);
+			if (!plussign)
+				modeParam = "*";
+			channel->broadcast(resMsg + mode  + " " + modeParam + "\r\n", nullptr, 0);
+			break;
+		case 'o':
+			if (!target)
+			{
+				sendError("401 " + nick + " " + modeParam + " :No such nick/channel", fd);
+				return;
+			}
+			if (channel->isClient(target) == -1)
+			{
+				sendError("441 " + nick + " " + modeParam + " " + chName + " :They aren't on that channel", fd);
+				return;
+			}
+			if (plussign && channel->isAdmin(target) == -1)
+				channel->addAdmin(target);
+			else if (!plussign && channel->isAdmin(target) >= 0)
+				channel->removeAdmin(target);
+			channel->broadcast(resMsg + mode + " " + modeParam + "\r\n", nullptr, 0);
+			break;
+		case 'l':
+			if (!plussign && channel->getUserLimit() == -1)
+				return ;
+			if (!modeParam.empty())
+			{
+				try {
+					int value = std::stoi(modeParam);
+					if (value < 1)
+						return ;
+				} catch (...) {
+					return ;
+				}
+			}
+			if (plussign)
+				channel->setUserLimit(std::stoi(modeParam));
+			else
+				channel->setUserLimit(-1);
+			if (modeParam[0] == '+')
+				modeParam = modeParam.substr(1);
+			channel->broadcast(resMsg + mode + " " + modeParam + "\r\n", nullptr, 0);
+			break;
+		case 'b':
+			break ;
+		default:
+		{
+			sendError("472 " + nick + " " + mode + " :is unknown mode char to me", fd);
+			break ;
 		}
 	}
-	std::cout << *channel << std::endl;
 }
 
 
